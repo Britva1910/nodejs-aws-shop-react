634edff53b8755090053982eff74511b710ef25f
Ivan Lashuk <bbritva1910@gmail.com>
2024-06-08 12:57:07 +0400

    feat: add CDK script for deploy

---
+++ cdk/cdk/lib/cdk-stack.ts
@@ -0,0 +1,62 @@
+import * as cdk from "aws-cdk-lib";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+import { Construct } from "constructs";
+
+export class CdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const cloudfrontOAI = new cdk.aws_cloudfront.OriginAccessIdentity(
+      this,
+      "AWS-task2"
+    );
+
+    const siteBucket = new s3.Bucket(this, "StaticBucket", {
+      bucketName: "aws-task2-cdk",
+      websiteIndexDocument: "index.html",
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    });
+
+    siteBucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        actions: ["S3:GetObject"],
+        resources: [siteBucket.arnForObjects("*")],
+        principals: [
+          new cdk.aws_iam.CanonicalUserPrincipal(
+            cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId
+          ),
+        ],
+      })
+    );
+
+    const distribution = new cdk.aws_cloudfront.CloudFrontWebDistribution(
+      this,
+      "AWS-CDK-distribution",
+      {
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: siteBucket,
+              originAccessIdentity: cloudfrontOAI,
+            },
+            behaviors: [
+              {
+                isDefaultBehavior: true,
+              },
+            ],
+          },
+        ],
+      }
+    );
+
+    new s3deploy.BucketDeployment(this, "CDK-bucket-deployment", {
+      sources: [s3deploy.Source.asset("../../dist")],
+      destinationBucket: siteBucket,
+      distribution,
+      distributionPaths: ["/*"],
+    });
+  }
+}
